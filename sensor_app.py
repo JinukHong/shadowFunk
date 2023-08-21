05414e4b33d599a0aa7af66d253f4a7852e6590f
Etherealxx <64251396+etherealxx@users.noreply.github.com>
2023-08-21 23:36:39 +0700

    to lightmode

--- sensor_app.py
+++ sensor_app.py
@@ -246,17 +246,17 @@ with tab3:
             write(chatwrite(translated_response))
 
 
-hide_footer_style = """
-<style>
-.reportview-container .main footer {visibility: hidden !important;}   
-#MainMenu {visibility: hidden !important;} 
-footer {visibility: hidden !important;} 
-div.block-container {padding-top:2rem;}
-.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-.styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-.viewerBadge_text__1JaDK {
-    display: none;
-}
-</style>
-"""
-st.markdown(hide_footer_style, unsafe_allow_html=True)
+# hide_footer_style = """
+# <style>
+# .reportview-container .main footer {visibility: hidden !important;}   
+# #MainMenu {visibility: hidden !important;} 
+# footer {visibility: hidden !important;} 
+# div.block-container {padding-top:2rem;}
+# .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+# .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+# .viewerBadge_text__1JaDK {
+#     display: none;
+# }
+# </style>
+# """
+# st.markdown(hide_footer_style, unsafe_allow_html=True)
